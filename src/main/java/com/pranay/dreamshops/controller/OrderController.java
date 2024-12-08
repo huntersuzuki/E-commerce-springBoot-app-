bb82fa7f0c9ae638976b7562a5844ec077992692
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-08 17:51:01 +0530

    Created the order controller

---
+++ src/main/java/com/pranay/dreamshops/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.pranay.dreamshops.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/orders")
+public class OrderController {
+}
