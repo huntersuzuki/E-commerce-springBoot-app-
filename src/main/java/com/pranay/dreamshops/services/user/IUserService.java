7e3b261d35b6beccdd8781c2cdcaad3d76726e34
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-08 17:49:19 +0530

    Created user service class & interface to implement the CRUD operations on user entity.

---
+++ src/main/java/com/pranay/dreamshops/services/user/IUserService.java
@@ -0,0 +1,15 @@
+package com.pranay.dreamshops.services.user;
+
+import com.pranay.dreamshops.model.User;
+import com.pranay.dreamshops.requests.CreateUserRequest;
+import com.pranay.dreamshops.requests.UserUpdateRequest;
+
+public interface IUserService {
+    User getUserbyId(Long userId);
+
+    User createUser(CreateUserRequest request);
+
+    User updateUser(UserUpdateRequest request, Long userId);
+
+    void deleteUser(Long userId);
+}
