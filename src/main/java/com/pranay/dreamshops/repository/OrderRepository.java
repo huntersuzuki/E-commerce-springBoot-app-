a01a2d4a5d8060b57509f5e5c0e77417f56b3646
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-08 17:54:05 +0530

    updated the order repository and created a custom query method

--- src/main/java/com/pranay/dreamshops/repository/OrderRepository.java
+++ src/main/java/com/pranay/dreamshops/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package com.pranay.dreamshops.repository;
 import com.pranay.dreamshops.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
 }
