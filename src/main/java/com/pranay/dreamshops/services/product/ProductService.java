62f8cce41899ac1b9e681a1f561e67e6a6804d99
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-15 18:29:43 +0530

    Added a custom helper method to check if the product already exists.

--- src/main/java/com/pranay/dreamshops/services/product/ProductService.java
+++ src/main/java/com/pranay/dreamshops/services/product/ProductService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import com.pranay.dreamshops.dto.ImageDto;
 import com.pranay.dreamshops.dto.ProductDto;
+import com.pranay.dreamshops.exceptions.AlreadyExistsException;
 import com.pranay.dreamshops.exceptions.ResourceNotFoundException;
 import com.pranay.dreamshops.model.Image;
 import com.pranay.dreamshops.repository.ImageRepository;
@@ -25,112 +26,121 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class ProductService implements IProductService {
 
-	@Autowired
-	private final ProductRepository productRepository;
-	private final CategoryRepository categoryRepository;
-	private final ImageRepository imageRepository;
-	private final ModelMapper modelMapper;
+    @Autowired
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final ImageRepository imageRepository;
+    private final ModelMapper modelMapper;
 
-	@Override
-	public Product addProduct(AddProductRequest request) {
+    @Override
+    public Product addProduct(AddProductRequest request) {
 
-		Category category = Optional.ofNullable(categoryRepository.findByName(request.getCategory().getName()))
-				.orElseGet(() -> {
-					Category newCategory = new Category(request.getCategory().getName());
-					return categoryRepository.save(newCategory);
-				});
-		request.setCategory(category);
-		return productRepository.save(createProduct(request, category));
-	}
+        if (productExists(request.getName(), request.getBrand())) {
+            throw new AlreadyExistsException(request.getBrand() + " " + request.getName() + " already exits, do you mean update the product?");
+        }
 
-	private Product createProduct(AddProductRequest request, Category category) {
+        Category category = Optional.ofNullable(categoryRepository.findByName(request.getCategory().getName()))
+                .orElseGet(() -> {
+                    Category newCategory = new Category(request.getCategory().getName());
+                    return categoryRepository.save(newCategory);
+                });
+        request.setCategory(category);
+        return productRepository.save(createProduct(request, category));
+    }
 
-		return new Product(request.getName(), request.getBrand(), request.getPrice(), request.getInventory(),
-				request.getDescription(), category);
-	}
+    private boolean productExists(String name, String brand) {
+        return productRepository.existsByNameAndBrand(name, brand);
+    }
 
-	@Override
-	public Long countProductsByBrandAndName(String brand, String name) {
 
-		return productRepository.countByBrandAndName(brand, name);
-	}
+    private Product createProduct(AddProductRequest request, Category category) {
 
-	@Override
-	public void deleteProductById(Long id) {
-		productRepository.findById(id).ifPresentOrElse(productRepository::delete, () -> {
-			throw new ResourceNotFoundException("Product not found");
-		});
+        return new Product(request.getName(), request.getBrand(), request.getPrice(), request.getInventory(),
+                request.getDescription(), category);
+    }
 
-	}
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
 
-	@Override
-	public List<Product> getAllProducts() {
+        return productRepository.countByBrandAndName(brand, name);
+    }
 
-		return productRepository.findAll();
-	}
+    @Override
+    public void deleteProductById(Long id) {
+        productRepository.findById(id).ifPresentOrElse(productRepository::delete, () -> {
+            throw new ResourceNotFoundException("Product not found");
+        });
 
-	@Override
-	public Product getProductById(Long id) {
+    }
 
-		return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found"));
-	}
+    @Override
+    public List<Product> getAllProducts() {
 
-	@Override
-	public List<Product> getProductByName(String name) {
+        return productRepository.findAll();
+    }
 
-		return productRepository.findByName(name);
-	}
+    @Override
+    public Product getProductById(Long id) {
 
-	@Override
-	public List<Product> getProductsByBrand(String name) {
-		return productRepository.findByBrand(name);
-	}
+        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
 
-	@Override
-	public List<Product> getProductsByCategory(String category) {
-		return productRepository.findByCategoryName(category);
-	}
+    @Override
+    public List<Product> getProductByName(String name) {
 
-	@Override
-	public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
-		return productRepository.findByCategoryNameAndBrand(category, brand);
-	}
+        return productRepository.findByName(name);
+    }
 
-	@Override
-	public List<Product> getProductsByBrandAndName(String brand, String name) {
-		return productRepository.findByBrandAndName(brand, name);
-	}
+    @Override
+    public List<Product> getProductsByBrand(String name) {
+        return productRepository.findByBrand(name);
+    }
 
-	@Override
-	public Product updateProduct(ProductUpdateRequest request, Long productId) {
-		return productRepository.findById(productId)
-				.map(existingProduct -> updateExistingProduct(existingProduct, request)).map(productRepository::save)
-				.orElseThrow(() -> new ResourceNotFoundException("Product not found."));
-	}
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategoryName(category);
+    }
 
-	private Product updateExistingProduct(Product existingProduct, ProductUpdateRequest request) {
-		existingProduct.setName(request.getName());
-		existingProduct.setBrand(request.getBrand());
-		existingProduct.setPrice(request.getPrice());
-		existingProduct.setInventory(request.getInventory());
-		existingProduct.setDescription(request.getDescription());
-		Category category = categoryRepository.findByName(request.getCategory().getName());
-		existingProduct.setCategory(category);
-		return existingProduct;
-	}
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return productRepository.findByCategoryNameAndBrand(category, brand);
+    }
 
-	@Override
-	public List<ProductDto> getConvertedProducts(List<Product> products) {
-		return products.stream().map(this::convertToDto).toList();
-	}
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return productRepository.findByBrandAndName(brand, name);
+    }
 
-	@Override
-	public ProductDto convertToDto(Product product) {
-		ProductDto productDto = modelMapper.map(product, ProductDto.class);
-		List<Image> images = imageRepository.findByProductId(product.getId());
-		List<ImageDto> imageDtos = images.stream().map(image -> modelMapper.map(image, ImageDto.class)).toList();
-		productDto.setImages(imageDtos);
-		return productDto;
-	}
+    @Override
+    public Product updateProduct(ProductUpdateRequest request, Long productId) {
+        return productRepository.findById(productId)
+                .map(existingProduct -> updateExistingProduct(existingProduct, request)).map(productRepository::save)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found."));
+    }
+
+    private Product updateExistingProduct(Product existingProduct, ProductUpdateRequest request) {
+        existingProduct.setName(request.getName());
+        existingProduct.setBrand(request.getBrand());
+        existingProduct.setPrice(request.getPrice());
+        existingProduct.setInventory(request.getInventory());
+        existingProduct.setDescription(request.getDescription());
+        Category category = categoryRepository.findByName(request.getCategory().getName());
+        existingProduct.setCategory(category);
+        return existingProduct;
+    }
+
+    @Override
+    public List<ProductDto> getConvertedProducts(List<Product> products) {
+        return products.stream().map(this::convertToDto).toList();
+    }
+
+    @Override
+    public ProductDto convertToDto(Product product) {
+        ProductDto productDto = modelMapper.map(product, ProductDto.class);
+        List<Image> images = imageRepository.findByProductId(product.getId());
+        List<ImageDto> imageDtos = images.stream().map(image -> modelMapper.map(image, ImageDto.class)).toList();
+        productDto.setImages(imageDtos);
+        return productDto;
+    }
 
 }
