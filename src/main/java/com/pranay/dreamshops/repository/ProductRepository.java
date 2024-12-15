0dc19e0d69236a85e9296356d3f1b432bf735995
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-15 18:29:11 +0530

    Added a custom query method to check is the product already exists.

--- src/main/java/com/pranay/dreamshops/repository/ProductRepository.java
+++ src/main/java/com/pranay/dreamshops/repository/ProductRepository.java
@@ -11,15 +11,17 @@ import java.util.List;
 @Qualifier("product")
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	List<Product> findByCategoryName(String category);
+    List<Product> findByCategoryName(String category);
 
-	List<Product> findByBrand(String brand);
+    List<Product> findByBrand(String brand);
 
-	List<Product> findByCategoryNameAndBrand(String category, String brand);
+    List<Product> findByCategoryNameAndBrand(String category, String brand);
 
-	List<Product> findByName(String name);
+    List<Product> findByName(String name);
 
-	List<Product> findByBrandAndName(String brand, String name);
+    List<Product> findByBrandAndName(String brand, String name);
 
-	Long countByBrandAndName(String brand, String name);
+    Long countByBrandAndName(String brand, String name);
+
+    boolean existsByNameAndBrand(String name, String brand);
 }
