0082424467ddf6537a2032f170cbb877d3ec81c2
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-12-15 18:16:17 +0530

    To avoid the recycled dependency UserDto class is implemented, and also it returns only the information which the user wants to see.

---
+++ src/main/java/com/pranay/dreamshops/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.pranay.dreamshops.dto;
+
+import com.pranay.dreamshops.model.Cart;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<OrderDto> orders;
+    private CartDto cart;
+}
