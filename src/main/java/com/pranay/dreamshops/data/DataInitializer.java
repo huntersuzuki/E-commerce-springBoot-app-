c33e5c71be413001009bbeb23f89a2be55f74bc2
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2025-01-15 19:49:14 +0530

    Temporary file which initializes the user and admin for testing.

--- src/main/java/com/pranay/dreamshops/data/DataInitializer.java
+++ src/main/java/com/pranay/dreamshops/data/DataInitializer.java
@@ -1,25 +1,35 @@
 package com.pranay.dreamshops.data;
 
+import com.pranay.dreamshops.model.Role;
 import com.pranay.dreamshops.model.User;
 import com.pranay.dreamshops.repository.UserRepository;
+import com.pranay.dreamshops.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
     private final UserRepository userRepository;
-
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        createDefaultUserIfNotExist();
+        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_CUSTOMER");
+        //createDefaultUserIfNotExist();
+        createDefaultRoleIfNotExists(defaultRoles);
+        //createDefaultAdminIfNotExist();
     }
 
 
     public void createDefaultUserIfNotExist() {
+        Role userRole = (Role) roleRepository.findByName("ROLE_CUSTOMER");
         for (int i = 1; i <= 5; i++) {
             String defaultEmail = "user" + i + "@gmail.com";
             if (userRepository.existsByEmail(defaultEmail)) {
@@ -29,11 +39,34 @@ public class DataInitializer implements ApplicationListener<ApplicationReadyEven
             user.setFirstName("Demo user" + i);
             user.setLastName("user" + i);
             user.setEmail(defaultEmail);
-            user.setPassword("123456");
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(userRole));
             userRepository.save(user);
             System.out.println("Default user " + i + " created successfully");
         }
     }
 
+    public void createDefaultAdminIfNotExist() {
+        Role adminRole = (Role) roleRepository.findByName("ROLE_ADMIN");
+        for (int i = 1; i <= 5; i++) {
+            String defaultEmail = "admin" + i + "@gmail.com";
+            if (userRepository.existsByEmail(defaultEmail)) {
+                continue;
+            }
+            User user = new User();
+            user.setFirstName("Demo admin" + i);
+            user.setLastName("admin" + i);
+            user.setEmail(defaultEmail);
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRoles(Set.of(adminRole));
+            userRepository.save(user);
+            System.out.println("Default admin " + i + " created successfully");
+        }
+    }
 
+    private void createDefaultRoleIfNotExists(Set<String> roles) {
+        roles.stream()
+                .filter(role -> roleRepository.findByName(role).isEmpty())
+                .map(Role::new).forEach(roleRepository::save);
+    }
 }
